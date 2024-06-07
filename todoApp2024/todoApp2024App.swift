7c9c1f1892ee4524ad37d6f293719d1bc78c8b74
jgarc121 <56375818+jgarc121@users.noreply.github.com>
2024-06-06 20:15:50 -0500

    Initial Commit

---
+++ todoApp2024/todoApp2024App.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp2024App.swift
+//  todoApp2024
+//
+//  Created by Jose Garcia on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
